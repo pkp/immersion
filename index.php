e3d47497d01ac68c5371102a295c2d6239dc7892
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2019-03-13 16:48:35 +0200

    Copyright year and coding style

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 /**
  * @file plugins/themes/immersion/index.php
  *
- * Copyright (c) 2014-2018 Simon Fraser University
- * Copyright (c) 2003-2018 John Willinsky
+ * Copyright (c) 2014-2019 Simon Fraser University
+ * Copyright (c) 2003-2019 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_themes_immersion
  * @brief Wrapper for Immersion theme plugin.
  *
  */
-	
+
 require_once('ImmersionThemePlugin.inc.php');
 
 return new ImmersionThemePlugin();
