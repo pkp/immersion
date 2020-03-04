31853db8d7c7f1581c3f227ad7a0a6872f24eb73
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2020-03-04 18:33:58 +0200

    #38 fix reviewer interests

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,23 +1,65 @@
 // initiating tag-it
 
-$(document).ready(function() {
-	$("#tagitInput").tagit();
+$("#tagitInput").each(function() {
+	var autocomplete_url = $(this).data("autocomplete-url");
+	$(this).tagit({
+		fieldName: $(this).data("field-name"),
+		allowSpaces: false,
+		autocomplete: {
+			source: function(request, response) {
+				$.ajax({
+					url: autocomplete_url,
+					data: {term: request.term},
+					dataType: "json",
+					success: function(jsonData) {
+						if (jsonData.status === true) {
+							response(jsonData.content);
+						}
+					}
+				});
+			}
+		}
+	});
 });
 
 (function () {
-	var checkbox = document.getElementById("checkbox-reviewer-interests");
-	if (checkbox != null) {
-		checkbox.onclick = function () {
-			var tagitInput = document.getElementById("reviewerInterests");
-			if (checkbox.checked === true) {
-				tagitInput.classList.remove("hidden");
-			} else {
-				tagitInput.classList.add("hidden");
+	/**
+	 * Determine if the user has opted to register as a reviewer
+	 *
+	 * @see: /templates/frontend/pages/userRegister.tpl
+	 */
+	function isReviewerSelected() {
+		var group = $("#reviewerOptinGroup").find("input");
+		var is_checked = false;
+		group.each(function() {
+			if ($(this).is(":checked")) {
+				is_checked = true;
+				return false;
 			}
+		});
+		
+		return is_checked;
+	}
+	
+	/**
+	 * Reveal the reviewer interests field on the registration form when a
+	 * user has opted to register as a reviewer
+	 *
+	 * @see: /templates/frontend/pages/userRegister.tpl
+	 */
+	function reviewerInterestsToggle() {
+		var is_checked = isReviewerSelected();
+		if (is_checked) {
+			$("#reviewerInterests").removeClass("hidden");
+		} else {
+			$("#reviewerInterests").addClass("hidden");
 		}
 	}
+	
+	// Update interests on page load and when the toggled is toggled
+	reviewerInterestsToggle();
+	$("#reviewerOptinGroup input").click(reviewerInterestsToggle);
 })();
-
 
 // Search form, wrapper for select tags
 
