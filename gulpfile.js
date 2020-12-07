8d777a2086a57071364cc31f47b5151d856783cb
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2020-12-07 14:08:52 +0200

    Dependencies update

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,51 @@ var minifyCSS = require('gulp-csso');
 var sourcemaps = require('gulp-sourcemaps');
 var minify = require('gulp-minify');
 
-gulp.task('sass', function () {
-	return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'node_modules/jquery-ui-dist/jquery-ui.min.css', 'node_modules/tag-it/css/jquery.tagit.css'])
+gulp.task('sass', function() {
+	return gulp
+		.src([
+			'node_modules/bootstrap/scss/bootstrap.scss',
+			'node_modules/jquery-ui-dist/jquery-ui.min.css',
+			'node_modules/tag-it/css/jquery.tagit.css'
+		])
 		.pipe(sass())
 		.pipe(concat('app.min.css'))
 		.pipe(minifyCSS())
-		.pipe(gulp.dest('resources/dist'))
+		.pipe(gulp.dest('resources/dist'));
 });
 
-gulp.task('scripts', function () {
-	return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/popper.js/dist/umd/popper.js', 'node_modules/bootstrap/dist/js/bootstrap.js', 'node_modules/jquery-ui-dist/jquery-ui.min.js', 'resources/js/tag-it.min.js', 'resources/js/main.js'])
+gulp.task('scripts', function() {
+	return gulp
+		.src([
+			'node_modules/jquery/dist/jquery.js',
+			'node_modules/popper.js/dist/umd/popper.js',
+			'node_modules/bootstrap/dist/js/bootstrap.js',
+			'node_modules/jquery-ui-dist/jquery-ui.min.js',
+			'resources/js/tag-it.min.js',
+			'resources/js/main.js'
+		])
 		.pipe(sourcemaps.init())
 		.pipe(concat('app.js'))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('resources/dist'))
+		.pipe(gulp.dest('resources/dist'));
 });
 
-gulp.task('compress', function () {
-	return gulp.src('resources/dist/app.js')
-		.pipe(minify({
-			ext: {
-				src: '-debug.js',
-				min: '.min.js'
-			}
-		}))
-		.pipe(gulp.dest('resources/dist'))
+gulp.task('compress', function() {
+	return gulp
+		.src('resources/dist/app.js')
+		.pipe(
+			minify({
+				ext: {
+					src: '-debug.js',
+					min: '.min.js'
+				}
+			})
+		)
+		.pipe(gulp.dest('resources/dist'));
 });
 
 gulp.task('compileAll', gulp.series('sass', 'scripts', 'compress'));
 
-gulp.task('watch', function () {
-	return gulp.watch('resources/js/**/*.js', gulp.series('scripts', 'compress'))
+gulp.task('watch', function() {
+	return gulp.watch('resources/js/**/*.js', gulp.series('scripts', 'compress'));
 });
